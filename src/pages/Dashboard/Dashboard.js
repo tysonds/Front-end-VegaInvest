12ced9c44904af3ab3eb595c79ecba2cd3009ea7
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-11-30 00:34:30 -0500

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,24 @@
-import react, { useState } from 'react'
+import React, { useState } from 'react';
 import './Dashboard.css';
+// import Plot from "react-plotly.js";
+import Plotly from 'plotly.js-basic-dist';
+import createPlotlyComponent from "react-plotly.js/factory";
+const Plot = createPlotlyComponent(Plotly);
 
 const Dashboard = () => {
+  return (
+<Plot
+        data={[
+          {
+            x: ['2021/09/01', '2021/09/02', '2021/09/03'],
+            y: [202, 206, 203],
+            type: 'linear',
+          },
+        ]}
+        layout={ {width: 900, height: 800, title: 'A Fancy Plot'} }
+      />
+  )
 
-return (
- <div> 
-   This is the Dashboard
-    </div>
-  );
 
 }
 export default Dashboard
