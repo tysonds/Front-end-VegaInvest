7e6a9accd848e792400a1c5a3d2627f03b5fada8
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-01 01:25:33 -0500

    Update PortfolioGraph.js

--- src/components/PortfolioGraph/PortfolioGraph.js
+++ src/components/PortfolioGraph/PortfolioGraph.js
@@ -14,9 +14,8 @@ const PortfolioGraph = () => {
                 {
                 x: ['2012-09-01', '2016-09-02', '2018-04-03','2018-09-03'],
                 y: [202, 500, 303, 300],
-                hovertemplate:  '<br><b>X</b>: %{x}<br>' +
-                                '<i>Price</i>: $%{y:.2f}' +
-                                '<b>%{text}</b>',
+                hovertemplate:  '%{x}<br>' +
+                                '$%{y:.2f}',
                 type: 'linear',
                 line:{width:5,color:"#0645AD"},
                 fill: 'tozeroy'
