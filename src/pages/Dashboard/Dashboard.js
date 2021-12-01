a8ecc19411ffa3a6c30f6f40016073dad74e6e73
tysonds <48194875+tysonds@users.noreply.github.com>
2021-11-30 22:43:40 -0500

    Cards here

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,6 +4,9 @@ import Dashhead from '../../components/Dashhead/Dashhead.js'
 // import Plot from "react-plotly.js";
 import PortfolioGraph from '../../components/PortfolioGraph/PortfolioGraph'
 import Footer from '../../components/Footer/Footer';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Dashboard = () => {
 
@@ -15,7 +18,28 @@ const Dashboard = () => {
    <div className="Dashcolcontain">
    <div className="Dashrowcontain">
       <PortfolioGraph></PortfolioGraph>
-   </div> 
+   </div>
+   <div className="Dashrowcontain">
+     <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title> "Retirement" </Card.Title>
+    <Card.Text>
+      $10,000 
+    </Card.Text>
+    <Button variant="primary"> View </Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title> "Education" </Card.Title>
+    <Card.Text>
+      $10,000
+    </Card.Text>
+    <Button variant="primary"> View </Button>
+  </Card.Body>
+</Card>
+<Button variant="primary"> Create New Portfolio </Button>
+     </div> 
    </div>
    <div>
    <Footer> </Footer>
