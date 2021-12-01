496ce5237d2d47414eb9890312ecd2f81a0c2fdb
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-11-30 20:18:09 -0500

    Update PortfolioGraph.js

--- src/components/PortfolioGraph/PortfolioGraph.js
+++ src/components/PortfolioGraph/PortfolioGraph.js
@@ -2,19 +2,52 @@ import Plotly from 'plotly.js-basic-dist';
 import createPlotlyComponent from "react-plotly.js/factory";
 const Plot = createPlotlyComponent(Plotly);
 
+
 const PortfolioGraph = () => {
 
     return (
     
         <div className="PortfolioGraph">
+
             <Plot
             data={[
                 {
-                x: ['2021/09/01', '2021/09/02', '2021/09/03'],
-                y: [202, 206, 203],
+                x: ['2012-09-01', '2016-09-02', '2018-04-03','2018-09-03'],
+                y: [202, 500, 303, 300],
                 type: 'linear',
                 }]}
-            layout={ {width: 450, height: 400, title: 'Portfolio'} }
+            layout={ 
+                {title: 'Portfolio Graph',
+                xaxis: {
+                  autorange: true,
+                  range: ["2019-09-01", "2018-11-01"],
+                  rangeselector: {buttons: [
+                      {
+                        count: 1,
+                        label: '1m',
+                        step: 'month',
+                        stepmode: 'backward'
+                      },
+                      {
+                        count: 6,
+                        label: '6m',
+                        step: 'month',
+                        stepmode: 'backward'
+                      },
+                      {
+                        count: 12,
+                        label: '1y',
+                        step: 'month',
+                        stepmode: 'backward'
+                      },
+                      {step: 'all'}
+                    ]},
+                },
+                yaxis: {
+                  autorange: true,
+                  range: [86.8700008333, 138.870004167],
+                  type: 'linear'
+                }} }
             />
         </div>
       );
