5d180c5b3235cf1e0452fc7dc6040c4baf60765d
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-02 02:28:07 -0500

    Update PortfolioSum.js

--- src/pages/PortfolioSum/PortfolioSum.js
+++ src/pages/PortfolioSum/PortfolioSum.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import './PortfolioSum.css';
+<<<<<<< Updated upstream
 import Porthead from '../../components/Porthead/Porthead';
+=======
+import ETF_table from '../../components/ETF_table/ETF_table';
+import Footer from '../../components/Footer/Footer';
+>>>>>>> Stashed changes
+
 
 
 const PortfolioSum = () => {
 
   return(
  <div className="Portsum"> 
+<<<<<<< Updated upstream
 <Porthead> </Porthead>
+=======
+      <ETF_table> </ETF_table>
+>>>>>>> Stashed changes
   </div>
   );
 
