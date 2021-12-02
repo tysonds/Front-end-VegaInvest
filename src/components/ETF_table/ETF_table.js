a4f8130e9c97ebc06c9646f4b171689083546fc5
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-02 03:08:35 -0500

    Update ETF_table.js

--- src/components/ETF_table/ETF_table.js
+++ src/components/ETF_table/ETF_table.js
@@ -1,46 +1,45 @@
 
 import react, { useState } from 'react'
-import * as Reactbootstrap from "react-bootstrap"
-
-const ETF = [
-    {ticker: "BBC", Value:"$5430.98", Shares:"2", Weight: "18.29%"},
-    {ticker: "GAMR", Value:"$1000.98", Shares:"3", Weight: "2.29%"},
-    {ticker: "QLNC", Value:"$1300.98", Shares:"4", Weight: "2.00%"},
-    {ticker: "GAMR", Value:"$1100.98", Shares:"14", Weight: "2.1%"}
-]
+import * as ReactBootStrap from "react-bootstrap"
+import './ETF_table.css'
 
 const ETF_table = () => { 
+    
+const ETF = [
+    {Ticker: "BBC", Value:"$5430.98", Shares:"2", Weight: "18.29%"},
+    {Ticker: "GAMR", Value:"$1000.98", Shares:"3", Weight: "2.29%"},
+    {Ticker: "QLNC", Value:"$1300.98", Shares:"4", Weight: "2.00%"},
+    {Ticker: "GAMR", Value:"$1100.98", Shares:"14", Weight: "2.1%"},
+]
 
     const ETF_render=(ETF,index)=> {
     return(
         <tr key={index}>
-            <td>{ETF.ticker}</td>
+            <td>{ETF.Ticker}</td>
             <td>{ETF.Value}</td>
             <td>{ETF.Shares}</td>
             <td>{ETF.Weight}</td>
         </tr>
-    );
+        )
     }
 
     return(
-        <div className="Table">
-            <Reactbootstrap.Table striped bordered hover>
-                <thread>
-                    <tr>
-                        <th>Ticker</th>
-                        <th>Value</th>
-                        <th>Shares</th>
-                        <th>Weight</th>
-                    </tr>
-                </thread>
-            </Reactbootstrap.Table>
-
-        </div>
-
-
-
-
-
-    );
+            <div className="tablefit">
+            <ReactBootStrap.Table striped bordered hover>
+            <thead>
+                <tr>
+                <th>Ticker</th>
+                <th>Value</th>
+                <th>Shares</th>
+                <th>Weight</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                {ETF.map(ETF_render)}
+                </tr>
+            </tbody>
+            </ReactBootStrap.Table> </div>
+            );
 }
-export default ETF_table
+export default ETF_table;
