1575f755f87445825e5c05eba326e507341fd55e
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-02 04:13:11 -0500

    aaa

--- src/components/ETF_table/ETF_table.js
+++ src/components/ETF_table/ETF_table.js
@@ -6,21 +6,22 @@ import './ETF_table.css'
 const ETF_table = () => { 
     
 const ETF = [
-    {ticker: "BBC", value:"$5430.98", shares:"2", weight: "18.29%"},
-    {ticker: "GAMR", value:"$1000.98", shares:"3", weight: "2.29%"},
-    {ticker: "QLNC", value:"$1300.98", shares:"4", weight: "2.00%"},
-    {ticker: "SPY", value:"$1100.98", shares:"14", weight: "2.1%"},
-    {ticker: "SPY", value:"$1100.98", shares:"14", weight: "2.1%"},
-    {ticker: "SPY", value:"$1100.98", shares:"14", weight: "2.1%"},
-    {ticker: "SPY", value:"$1100.98", shares:"14", weight: "2.1%"},
+    {ticker: "BBC", description:"Tracks the S&P500 stock market index of large and mid-cap US stocks",shares:"2", value:"$5430.98", weight: "18.29%"},
+    {ticker: "GAMR", description:"Tracks the S&P500 stock market index of large and mid-cap US stocks", shares:"3",value:"$1000.98", weight: "2.29%"},
+    {ticker: "QLNC", description:"Tracks the S&P500 stock market index of large and mid-cap US stocks", shares:"4",value:"$1300.98", weight: "2.00%"},
+    {ticker: "SPY", description:"Tracks the S&P500 stock market index of large and mid-cap US stocks", shares:"14",value:"$1100.98", weight: "2.1%"},
+    {ticker: "SPY", description:"Tracks the S&P500 stock market index of large and mid-cap US stocks", shares:"14",value:"$1100.98", weight: "2.1%"},
+    {ticker: "SPY", description:"Tracks the S&P500 stock market index of large and mid-cap US stocks", shares:"14",value:"$1100.98", weight: "2.1%"},
+    {ticker: "SPY", description:"Tracks the S&P500 stock market index of large and mid-cap US stocks", shares:"14",value:"$1100.98", weight: "2.1%"},
 ]
 
     const renderETF=(ETF,index)=> {
     return(
         <tr key={index}>
             <td>{ETF.ticker}</td>
-            <td>{ETF.value}</td>
+            <td>{ETF.description}</td>
             <td>{ETF.shares}</td>
+            <td>{ETF.value}</td>
             <td>{ETF.weight}</td>
         </tr>
         )
@@ -35,9 +36,10 @@ const ETF = [
             <thead>
                 <tr>
                 <th>Ticker</th>
+                <th>Description</th>
                 <th>Value</th>
                 <th>Shares</th>
-                <th>Weight</th>
+                <th>% of Account</th>
                 </tr>
             </thead>
             <tbody>
