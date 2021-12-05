ff639e353d66451276d1613133e78e2cf3abfd33
VegaInvest <92478044+VegaInvest@users.noreply.github.com>
2021-12-04 19:40:44 -0500

    Update Login.js

--- src/components/LoginForm/Login.js
+++ src/components/LoginForm/Login.js
@@ -1,8 +1,11 @@
-import react, { useState } from 'react'
+import react, { useState,useEffect } from 'react'
 import './Login.css'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+
+
 
 const Login = () => {
 
@@ -13,12 +16,48 @@ const [values, setValues] = useState(
     }
 );
 
+const [getPostMessage, setGetPostMessage] = useState({});
+
 const handleEmailChange = (event) => {
     setValues({...values, email: event.target.value})
 }
 const handlePasswordInputChange = (event) => {
     setValues({...values, password: event.target.value})
 }
+
+let navigate = useNavigate();
+
+useEffect(() => {
+    if (getPostMessage.Status=='found') {
+        console.log("inside")
+        navigate("../dashboard", {});
+      }
+}, [getPostMessage])
+
+
+ const HandleLogin = () => {
+    var fullurl='http://127.0.0.1:5000/users/login';
+    axios.post(fullurl, values).then(response => {
+        setGetPostMessage(response.data)
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+}
+
+{/*const HandleLogin = () => {
+    var fullurl='http://127.0.0.1:5000/users/login';
+    axios.post(fullurl, values).then(response => {
+        setGetPostMessage(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+      if (getPostMessage.Status=='found') {
+        console.log("SUCCESS")
+      }
+}*/}
+
+
 return (
 
     <div className="Login-container">
@@ -47,10 +86,10 @@ return (
             <br />
         <a href="">Forgot Password? </a>
         <br />
-        <Link to="/dashboard" style={{ textDecoration: 'none', color: 'black' }}> 
-        <Button variant="secondary" className="LoginBtn" id="BtnLogin"> Log In </Button>
+        {/* <Link to="/dashboard" style={{ textDecoration: 'none', color: 'black' }}> */}
+        <Button onClick={HandleLogin} variant="secondary" className="LoginBtn" id="BtnLogin"> Log In </Button>
         <br />
-        </Link>
+        {/* </Link> */}
         <Link to="/createuser" style={{ textDecoration: 'none', color: 'black' }}> 
         <Button variant="dark" className="LoginBtn" id="SignUp"> 
         Sign Up 
